86765b8ee780e00fb391f03ea2fb11e67941801c
darinisawesome <github@darinisawesome.com>
2017-07-20 20:32:16 -0700

    Reupload (Initial Commit)
    Includes:
    R Markdown document of my goal with this project,
    Dataset I'll be working with (pretty clean, but I'll clean as necessary and track those changes and code),
    R script that loads that dataset,
    Project file for R Studio.

---
+++ VGS Dataset Refinement.R
@@ -0,0 +1,9 @@
+#load necessary libraries
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(tidyr)
+
+#load video game sales dataset
+vgdf <- read_csv("~/Github/Springboard_Foundations_Capstone/Video_Games_Sales_as_at_22_Dec_2016.csv")
+
