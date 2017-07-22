4c6a8367387a1bb51722eb46ec807fe18ea35064
darinisawesome <github@darinisawesome.com>
2017-07-22 12:27:49 -0700

    Rename
    I changed the name of the VGS dataset, and converted the original to an easier to read Excel file too in case that becomes somehow preferential.

--- VGS Dataset Refinement.R
+++ VGS Dataset Refinement.R
@@ -5,5 +5,5 @@ library(readr)
 library(tidyr)
 
 #load video game sales dataset
-vgdf <- read_csv("~/Github/Springboard_Foundations_Capstone/Video_Games_Sales_as_at_22_Dec_2016.csv")
+vgdf <- read_csv("~/Github/Springboard_Foundations_Capstone/Video_Games_Sales_as_at_22_Dec_2016_original.csv")
 
